1450b973b357a4c2b29bd1e07e7f75bcdfadb203
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2021-11-22 11:34:16 +0100

    changes in redux comments

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,7 +1,7 @@
 import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
 
-// Add other imports and bind them to action creators - when it improves
+// **Add other imports and bind them to action creators - when it enlarges**
 import {
   loginUserActionCreators
 } from "../redux";
--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import loginReducer from "./loginReducer";
 
+// **Add here another reducers, that has to be persisted in localStorage of the user's browser**
 const reducers = combineReducers({
     login: loginReducer
 });
