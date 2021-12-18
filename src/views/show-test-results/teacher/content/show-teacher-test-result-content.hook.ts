0b976319b4d495aa303262f50a40ba87d8ef6e64
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2021-12-18 15:27:12 +0100

    added creating CSV and PDF documents

--- src/views/show-test-results/teacher/content/show-teacher-test-result-content.hook.ts
+++ src/views/show-test-results/teacher/content/show-teacher-test-result-content.hook.ts
@@ -1,9 +1,14 @@
 import {useState, useEffect} from "react";
 import {useTypedSelector} from "../../../../hooks/useTypedSelector";
 
+// jsPDF
+import '../../../../assets/fonts/PTSans-normal';
+import { jsPDF } from "jspdf";
+
 // functions
 import formatTestDate from "../../../../functions/format-test-date";
 import isUserType from "../../../../functions/is-user-type";
+import translateQuestionType from "../../../../functions/translate-question-type";
 
 // interfaces
 import {IStudentData} from "./show-teacher-test-result-content.types";
@@ -134,14 +139,187 @@ const useShowTeacherTestResultContent = (testId :string) => {
   };
 
   const generateCSVFile = () => {
+    let testName = "";
+    let separator = ";";
+    let csvContent = "data:text/csv;charset=utf-8,";
 
+    // Header of the CSV
+    csvContent += "Legenda informacji ogólnych o teście\n";
+    csvContent += `Lp.${separator}Imię i nazwisko ucznia${separator}Email ucznia${separator}Nazwa użytkownika ucznia${separator}Nazwa testu${separator}Data wykonania testu${separator}Maksymalna ilość punktów za test${separator}Zdobyta ilość punktów\n\n`;
+    csvContent += "Legenda pytań i odpowiedzi\n";
+    csvContent += `Lp.${separator}Treść pytania${separator}Typ pytania${separator}Maksymalne punkty${separator}Zdobyte punkty${separator}Prawidłowe odpowiedzi${separator}Odpowiedzi studenta${separator}Odpowiedź otwarta studenta\n\n`;
+
+    // Adding the result for each student
+    resultsArray.forEach((result, indexOne) => {
+      // Setting test name to use in file name
+      if (indexOne === 0) {
+        testName = result.name;
+      }
+
+      // Search for the student with specified id
+      let foundStudent = students.find(student => student.id === result.userId);
+
+      // If student is found, fill the csv string with the content
+      if (foundStudent) {
+        csvContent += "INFORMACJE OGÓLNE\n";
+        csvContent += `${(indexOne+1).toString()}${separator}${foundStudent.firstName} ${foundStudent.lastName}${separator}${foundStudent.email}${separator}${foundStudent.userName}${separator}${result.name}${separator}${result.dateOfExecution}${separator}${result.maxPoints}${separator}${result.totalPoints}\n`;
+        csvContent += "PYTANIE I ODPOWIEDZI\n";
+
+        result.answers.forEach((answer, indexTwo) => {
+          csvContent += `${(indexTwo+1).toString()}${separator}${answer.questionText}${separator}${translateQuestionType(answer.questionType)}${separator}${answer.questionPoints}${separator}${answer.answerRatedPoints}${separator}${answer.questionType !== "DESCRIPTIVE" ? answer.correctAnswers.filter(correctAnswer => correctAnswer.correct).map(correctAnswer => correctAnswer.answer).join("*") : "-"}${separator}${answer.questionType !== "DESCRIPTIVE" ? answer.userAnswers.map(userAnswer => userAnswer.answer).join("*") : "-"}${separator}${answer.questionType === "DESCRIPTIVE" ? answer.descriptiveAnswerText : "-"}\n`;
+        });
+
+        csvContent += "\n";
+      }
+    });
+
+    // Create the CSV file and download it
+    let encodedUri = encodeURI(csvContent);
+    let link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", `${testName}_wyniki.csv`);
+    document.body.appendChild(link); // Required for FF
+
+    link.click(); // This will download the CSV data file
   };
 
   const generatePDFFile = () => {
+    let testName = "";
 
+    // Variables of the document
+    // A4 format in px is 595 x 842
+    const maxPageWidth = 595;
+    const maxPageHeight = 842;
+    const pageMargin = 20;
+    const headerSpacingBottom = 20;
+    const normalTextSpacingBottom = 17;
+
+    // Variables of the cursor position
+    let cursorX = pageMargin;
+    let cursorY = pageMargin;
+
+    // Local function for checking if the cursor is out of the working area - if so it resets the cursor position
+    const checkTheCursorPosition = () => {
+      if (cursorX >= (maxPageWidth - pageMargin)) {
+        cursorX = pageMargin;
+      }
+
+      if (cursorY >= (maxPageHeight - pageMargin)) {
+        doc.addPage("a4", "portrait");
+        cursorX = pageMargin;
+        cursorY = pageMargin;
+      }
+    };
+
+    const doc = new jsPDF({
+      orientation: "portrait",
+      unit: "pt",
+      format: "a4"
+    });
+
+    // Set default font
+    doc.setFont("PTSans");
+
+    // Fill the document with each student data
+    resultsArray.forEach((result, indexOne) => {
+      // Setting test name to use in file name
+      if (indexOne === 0) {
+        testName = result.name;
+      }
+
+      // Search for the student with specified id
+      let foundStudent = students.find(student => student.id === result.userId);
+
+      // If student is found, fill the pdf with the content
+      if (foundStudent) {
+        doc.setFontSize(16);
+        doc.text(`${indexOne+1}. Informacje o teście - ${foundStudent.firstName} ${foundStudent.lastName}`, cursorX, cursorY);
+        cursorY += headerSpacingBottom;
+
+        doc.line(cursorX, cursorY, maxPageWidth-pageMargin, cursorY, "F");
+        cursorY += headerSpacingBottom + 1;
+
+        doc.setFontSize(12);
+        doc.text(`Rozwiązujący uczeń: ${foundStudent.firstName} ${foundStudent.lastName}\n`, cursorX, cursorY);
+        cursorY += normalTextSpacingBottom;
+
+        doc.text(`Nazwa użytkownika ucznia: ${foundStudent.userName}\n`, cursorX, cursorY);
+        cursorY += normalTextSpacingBottom;
+
+        doc.text(`Email ucznia: ${foundStudent.email}\n`, cursorX, cursorY);
+        cursorY += normalTextSpacingBottom;
+
+        doc.text(`Nazwa testu: ${result.name}\n`, cursorX, cursorY);
+        cursorY += normalTextSpacingBottom;
+
+        doc.text(`Data wykonania: ${result.dateOfExecution}\n`, cursorX, cursorY);
+        cursorY += normalTextSpacingBottom;
+
+        doc.text(`Maksymalna ilość punktów do zdobycia: ${result.maxPoints}\n`, cursorX, cursorY);
+        cursorY += normalTextSpacingBottom;
+
+        doc.text(`Zdobyte punkty: ${result.totalPoints}\n`, cursorX, cursorY);
+        cursorY += headerSpacingBottom*2;
+
+        doc.setFontSize(16);
+        doc.text("Odpowiedzi na pytania", cursorX, cursorY);
+        cursorY += headerSpacingBottom;
+
+        doc.line(cursorX, cursorY, maxPageWidth-pageMargin, cursorY, "F");
+        cursorY += headerSpacingBottom + 1;
+
+        result.answers.forEach((answer, indexTwo) => {
+          checkTheCursorPosition();
+
+          doc.setFontSize(14);
+          doc.text(`${indexTwo+1}. ${answer.questionText}`, cursorX, cursorY);
+          cursorY += headerSpacingBottom;
+          checkTheCursorPosition();
+
+          // Question
+          doc.setFontSize(12);
+          doc.text(`Typ pytania: ${translateQuestionType(answer.questionType)}`, cursorX, cursorY);
+          cursorY += normalTextSpacingBottom;
+          checkTheCursorPosition();
+
+          doc.text(`Zdobyte punkty: ${answer.answerRatedPoints} / ${answer.questionPoints}`, cursorX, cursorY);
+          cursorY += normalTextSpacingBottom;
+          checkTheCursorPosition();
+
+          doc.text(`Prawidłowe odpowiedzi: ${answer.questionType !== "DESCRIPTIVE" ? answer.correctAnswers.filter(correctAnswer => correctAnswer.correct).map(correctAnswer => correctAnswer.answer).join(", ") : "-"}`, cursorX, cursorY);
+          cursorY += normalTextSpacingBottom*2;
+          checkTheCursorPosition();
+
+          doc.text(`Odpowiedzi studenta: ${answer.questionType !== "DESCRIPTIVE" ? answer.userAnswers.map(userAnswer => userAnswer.answer).join(", ") : "-"}`, cursorX, cursorY);
+          cursorY += normalTextSpacingBottom*2;
+          checkTheCursorPosition();
+
+          doc.text(`Otwarta odpowiedź studenta: ${answer.questionType === "DESCRIPTIVE" ? answer.descriptiveAnswerText : "-"}`, cursorX, cursorY);
+          cursorY += normalTextSpacingBottom*2;
+          checkTheCursorPosition();
+
+          if (indexTwo < (result.answers.length-1)) {
+            doc.setDrawColor(169, 169, 169);
+            doc.line(cursorX, cursorY, maxPageWidth-pageMargin, cursorY);
+            cursorY += normalTextSpacingBottom + 1;
+          }
+        });
+
+        // Add next page if it's not the last student
+        if (indexOne !== resultsArray.length-1) {
+          cursorX = pageMargin;
+          cursorY = pageMargin;
+          doc.addPage("a4", "portrait");
+        }
+      }
+    });
+
+    // Save the pdf document and download it
+    doc.save(`${testName}_wyniki.pdf`);
   };
 
   const generateDOCXFile = () => {
+
 
   };
 
